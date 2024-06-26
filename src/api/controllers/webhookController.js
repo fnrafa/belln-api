dbd3b7e1faf48f730cb72f44d288e3d50228f317
fnrafa <fikrinoor790@gmail.com>
2024-06-26 13:37:10 +0700

    Fix: fix payment error

--- src/api/controllers/webhookController.js
+++ src/api/controllers/webhookController.js
@@ -57,7 +57,7 @@ exports.handleWebhook = async (req, res) => {
         default:
     }*/
     const event = req.body;
-    console.log(event.type);
+    console.log(event);
     switch (event.type) {
         case 'payment_intent.succeeded':
             const paymentIntent = event.data.object;
